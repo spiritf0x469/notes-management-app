c21995ba764cd99a78104e5419cdc0a82b104815
sameer sainii <justanid2006@gmail.com>
2026-06-20 19:41:18 +0530

    initial commit

---
+++ website/models.py
@@ -0,0 +1,16 @@
+from .import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.DateTime(timezone=True),default=func.now())
+    data=db.Column(db.String(10000))
+    user_id=db.Column(db.Integer,db.ForeignKey('user.id'))
+
+class User(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(150),unique=True)
+    password=db.Column(db.String(150))
+    name=db.Column(db.String(150))
+    notes=db.relationship('Note')
